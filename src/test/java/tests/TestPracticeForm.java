abce9ad6db20808ca5f3cbe424707f65398534ee
Tverskoy31 <medvedev-qa@mail.ru>
2024-03-11 23:11:41 +0300

    удалил: -Configuration.holdBrowserOpen = true; -Configuration.timeout -AfterALL

--- src/test/java/tests/TestPracticeForm.java
+++ src/test/java/tests/TestPracticeForm.java
@@ -1,7 +1,6 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -15,13 +14,8 @@ public class TestPracticeForm {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 5000; // default 4000
     }
-    @AfterAll
-    static void after() {
-        Configuration.holdBrowserOpen = true;
-    }
+
     @Test
     void successFormTest() {
         open("/automation-practice-form");
